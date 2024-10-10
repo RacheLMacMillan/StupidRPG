dc21d8dca42ce2cb2af1e209d96f5a9e5aa1efd8
Mitchell <r4chel.1997@gmail.com>
2024-10-11 10:27:42 +1100

    Deleted a Trash

--- Assets/Scripts/Player/Input/PlayerInput.cs
+++ Assets/Scripts/Player/Input/PlayerInput.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
 	[SerializeField] private PlayerMover _playerMover;
-	[SerializeField] private PlayerDasher _playerDasher;
 	
 	private InputMap _inputMap;
 	
@@ -12,9 +11,6 @@ public class PlayerInput : MonoBehaviour
 		_inputMap = new InputMap();
 		
 		_playerMover = GetComponent<PlayerMover>();
-		_playerDasher = GetComponent<PlayerDasher>();
-		
-		_inputMap.PlayScene.Dash.performed += context => _playerDasher.Dash(_inputMap.PlayScene.Move.ReadValue<Vector2>());
 	}
 	
 	private void OnEnable()
