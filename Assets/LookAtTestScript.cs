beddbe412680637690733c0164a0b4ef51a8317c
Mitchell <r4chel.1997@gmail.com>
2024-10-17 21:30:40 +1100

    Add Assets

--- Assets/LookAtTestScript.cs
+++ Assets/LookAtTestScript.cs
@@ -1,19 +1,25 @@
+using System;
 using UnityEngine;
 
 public class LookAtTestScript : MonoBehaviour
 {
-	[SerializeField] private CameraFollower _cameraFollower;
 	[SerializeField] private GameObject _target;
 	[SerializeField] private float _time;
 	[SerializeField] private float _smoothValue;
 	
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		_cameraFollower.ChangeTargetForSomeTime(_target, _time, _smoothValue);
-	}
-	
-	private void OnDestroy()
-	{
-		Destroy(gameObject);
+		CameraFollower cameraFollower = other.GetComponent<CameraFollower>();
+		
+		if (cameraFollower == null)
+		{
+			throw new NullReferenceException();
+		}
+		else
+		{
+			cameraFollower.ChangeTargetForSomeTime(_target, _time, _smoothValue);
+			
+			gameObject.SetActive(false);
+		}
 	}
 }
