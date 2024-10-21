e282b308a5b4c4b32ccfb8a4a3aea1d3e1bee7c8
Mitchell <r4chel.1997@gmail.com>
2024-10-21 20:55:24 +1100

    Update TreantScript

--- Assets/Scripts/Enemies/Treant/TreantScript.cs
+++ Assets/Scripts/Enemies/Treant/TreantScript.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
 
-public class TreantScript : MonoBehaviour
+public class TreantScript : MonoBehaviour, IMovable, IAttackable, IDestructible
 {
+	public void Move()
+	{
+		
+	}
 	
+	public void Attack()
+	{
+		
+	}
+	
+	public void Destroy()
+	{
+		
+	}
 }
